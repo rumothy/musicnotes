8e958b6613d957d9281785e554247ce02f02283f
rumothy <rumothy@yahoo.com>
2020-04-10 14:12:16 -0400

    Add test

--- src/test/api.test.js
+++ src/test/api.test.js
@@ -1,10 +1,32 @@
 import API from "../utils/API";
+const chai = require("chai");
+const chaiHttp = require("chai-http");
 const expect = require("chai").expect;
 
+chai.use(chaiHttp);
 describe("getTechniques", () => {
   it("should return an array of three techniques", () => {
     API.getTechniques()
-      .then(data => expect(data).to.be.an("array"))
+      .then(data =>
+        expect(data)
+          .to.be.an("array")
+          .that.has.lengthOf(3)
+      )
       .catch(err => console.log(err));
+  });
+});
+
+describe("getTechniques", () => {
+  it("should return an object named technique1", () => {
+    API.getTechniques().then(techniques => {
+      expect(techniques[0])
+        .to.be.an("object")
+        .that.includes({ _id: 1, name: "technique1" });
+
+      const points = techniques[0].points;
+      expect(points)
+        .to.be.an("array")
+        .that.has.lengthOf(3);
+    });
   });
 });
